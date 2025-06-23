4e0a3c689419604706509ccbfa7c4aab5bce9cd9
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-06-23 08:28:44 +0800

    [feat] update ui.

--- AndroidMvvm01GetLocalData/app/src/main/java/com/example/androidmvvm01getlocaldata/MainActivity.kt
+++ AndroidMvvm01GetLocalData/app/src/main/java/com/example/androidmvvm01getlocaldata/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidmvvm01getlocaldata.ui.theme.AndroidMvvm01GetLocalDataTheme
+import com.example.androidmvvm01getlocaldata.view.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidMvvm01GetLocalDataTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier) { innerPadding ->
+                    HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidMvvm01GetLocalDataTheme {
-        Greeting("Android")
     }
 }
---
+++ AndroidMvvm01GetLocalData/app/src/main/java/com/example/androidmvvm01getlocaldata/view/HomePage.kt
@@ -0,0 +1,23 @@
+package com.example.androidmvvm01getlocaldata.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {}) {
+            Text(text = "Get Data")
+        }
+    }
+}
