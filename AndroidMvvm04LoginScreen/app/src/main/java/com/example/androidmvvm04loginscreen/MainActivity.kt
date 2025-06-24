67bd7b4f00c03058485205c2e9049d7d39327cdc
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-06-24 09:07:05 +0800

    Create app.

---
+++ AndroidMvvm04LoginScreen/app/src/main/java/com/example/androidmvvm04loginscreen/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidmvvm04loginscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidmvvm04loginscreen.ui.theme.AndroidMvvm04LoginScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidMvvm04LoginScreenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidMvvm04LoginScreenTheme {
+        Greeting("Android")
+    }
+}
