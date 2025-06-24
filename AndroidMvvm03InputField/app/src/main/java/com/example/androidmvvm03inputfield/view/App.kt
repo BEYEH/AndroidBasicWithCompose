502431d25d785283ed6064e9a37aed84c80a13fe
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-06-24 08:36:44 +0800

    [feat] add TextFieldValue.

--- AndroidMvvm03InputField/app/src/main/java/com/example/androidmvvm03inputfield/view/App.kt
+++ AndroidMvvm03InputField/app/src/main/java/com/example/androidmvvm03inputfield/view/App.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.androidmvvm03inputfield.viewmodel.AppViewModel
 
@@ -18,6 +19,9 @@ import com.example.androidmvvm03inputfield.viewmodel.AppViewModel
 fun App(modifier: Modifier, viewModel: AppViewModel) {
     val inputTextState = viewModel.inputText.observeAsState("")
     val inputText = inputTextState.value
+
+    val inputTextFieldValueState = viewModel.inputTextFieldValue.observeAsState(TextFieldValue(""))
+    val inputTextFieldValue = inputTextFieldValueState.value
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -27,11 +31,19 @@ fun App(modifier: Modifier, viewModel: AppViewModel) {
         OutlinedTextField(
             value = inputText,
             onValueChange = { viewModel.onTextChanged(it) },
-            label = { Text("Input") }
-        )
-
-        Spacer(modifier = modifier.height(16.dp))
+            label = { Text("Input as string") })
 
         Text(text = "Your input is：$inputText")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = inputTextFieldValue,
+            onValueChange = { viewModel.onTextFieldValueChanged(it) },
+            label = { Text("Input as TextFieldValue") })
+
+        Text(text = "Value of TextFieldValue：${inputTextFieldValue}")
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Text of TextFieldValue：${inputTextFieldValue.text}")
     }
 }
--- AndroidMvvm03InputField/app/src/main/java/com/example/androidmvvm03inputfield/viewmodel/AppViewModel.kt
+++ AndroidMvvm03InputField/app/src/main/java/com/example/androidmvvm03inputfield/viewmodel/AppViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.androidmvvm03inputfield.viewmodel
 
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,7 +9,16 @@ class AppViewModel : ViewModel() {
     private val _inputText = MutableLiveData("")
     val inputText: LiveData<String> = _inputText
 
+    private val _inputTextFieldValue = MutableLiveData(
+        TextFieldValue("")
+    )
+    val inputTextFieldValue: LiveData<TextFieldValue> = _inputTextFieldValue
+
     fun onTextChanged(newText: String) {
         _inputText.value = newText
+    }
+
+    fun onTextFieldValueChanged(newValue: TextFieldValue) {
+        _inputTextFieldValue.value = newValue
     }
 }
