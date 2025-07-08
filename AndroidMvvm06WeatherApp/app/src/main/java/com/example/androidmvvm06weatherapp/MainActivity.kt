88fb15c4d32383503b62f91b4759345d82049a2e
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-07-09 07:30:22 +0800

    [feat] update ui.

--- AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/MainActivity.kt
+++ AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidmvvm06weatherapp.ui.theme.AndroidMvvm06WeatherAppTheme
 import com.example.androidmvvm06weatherapp.view.App
@@ -18,7 +22,12 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AndroidMvvm06WeatherAppTheme {
-                App(appViewModel)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App(appViewModel)
+                }
             }
         }
     }
--- AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
+++ AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -17,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.androidmvvm06weatherapp.viewmodel.AppViewModel
 
 @Composable
@@ -29,12 +32,16 @@ fun AppScreen() {
     var city by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
             OutlinedTextField(
+                modifier = Modifier.weight(1f),
                 value = city,
                 onValueChange = { city = it },
                 label = { Text(text = "Search for any location") }
