6e224e4f09da931242bd7079a21fa23b8abfab1f
kells\KellsPing <kellsping@gmail.com>
2025-07-09 06:21:05 +0800

    [feat] update ui.

--- AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
+++ AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
@@ -2,9 +2,19 @@ package com.example.androidmvvm06weatherapp.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.androidmvvm06weatherapp.viewmodel.AppViewModel
@@ -16,11 +26,28 @@ fun App(appViewModel: AppViewModel) {
 
 @Composable
 fun AppScreen() {
+    var city by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Hello Kotlin")
+        Row {
+            OutlinedTextField(
+                value = city,
+                onValueChange = { city = it },
+                label = { Text(text = "Search for any location") }
+            )
+
+            IconButton(onClick = {
+                /*TODO*/
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "search"
+                )
+            }
+        }
     }
 }
