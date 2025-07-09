1ceb6e1ae7a63ce9b01ec6700568cc2493d70e57
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-07-09 10:23:10 +0800

    [feat] update result in UI.

--- AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
+++ AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/view/App.kt
@@ -8,18 +8,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.androidmvvm06weatherapp.api.NetworkResponse
 import com.example.androidmvvm06weatherapp.viewmodel.AppViewModel
 
 @Composable
@@ -30,13 +33,18 @@ fun App(appViewModel: AppViewModel) {
 @Composable
 fun AppScreen(appViewModel: AppViewModel) {
     var city by remember { mutableStateOf("") }
+    val weatherResult = appViewModel.weatherResult.observeAsState()
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(8.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
@@ -55,6 +63,22 @@ fun AppScreen(appViewModel: AppViewModel) {
                     contentDescription = "search"
                 )
             }
+        }
+
+        when (val result = weatherResult.value) {
+            is NetworkResponse.Error -> {
+                Text(text = result.message)
+            }
+
+            NetworkResponse.Loading -> {
+                CircularProgressIndicator()
+            }
+
+            is NetworkResponse.Success<*> -> {
+                Text(text = result.data.toString())
+            }
+
+            null -> {}
         }
     }
 }
--- AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/viewmodel/AppViewModel.kt
+++ AndroidMvvm06WeatherApp/app/src/main/java/com/example/androidmvvm06weatherapp/viewmodel/AppViewModel.kt
@@ -1,26 +1,45 @@
 package com.example.androidmvvm06weatherapp.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.androidmvvm06weatherapp.api.Constant
+import com.example.androidmvvm06weatherapp.api.NetworkResponse
 import com.example.androidmvvm06weatherapp.api.RetrofitInstance
+import com.example.androidmvvm06weatherapp.api.WeatherModel
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 class AppViewModel : ViewModel() {
     private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableLiveData<NetworkResponse<WeatherModel>>()
+    val weatherResult: LiveData<NetworkResponse<WeatherModel>> = _weatherResult
 
     fun getData(city: String) {
+        _weatherResult.value = NetworkResponse.Loading
         viewModelScope.launch {
-            val response = weatherApi.getWeather(
-                Constant.apiKey,
-                city
-            )
-
-            if (response.isSuccessful){
-                Log.d("appViewModel", "Response: ${response.body().toString()}")
-            }else{
-                Log.d("appViewModel", "Errors: ${response.message()}")
+            try {
+                val response = weatherApi.getWeather(
+                    Constant.apiKey,
+                    city
+                )
+                if (response.isSuccessful) {
+                    Log.d("appViewModel", "Response: ${response.body().toString()}")
+                    response.body()?.let {
+                        _weatherResult.value = NetworkResponse.Success(it)
+                    }
+                } else {
+                    Log.d("appViewModel", "Errors: ${response.message()}")
+                    _weatherResult.value = NetworkResponse.Error("Failed to load data.")
+                }
+            } catch (e: IOException) {
+                Log.e("Network", "Network error: ${e.message}")
+                _weatherResult.value = NetworkResponse.Error("Failed to load data.")
+            } catch (e: Exception) {
+                Log.e("General", "Unknown error: ${e.message}")
+                _weatherResult.value = NetworkResponse.Error("Failed to load data.")
             }
         }
     }
